de0b1d8ccc91ea767678eb3b96b3a82a507c81e5
Nguyen Minh Hieu <20130261@st.hcmuaf.edu.vn>
2026-05-30 20:53:52 -1000

    di chuyển nhân vật bằng phím a d

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,24 +1,40 @@
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import model.Player;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.Timer;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener{
 
     private Player player;
+    private boolean leftPressed = false;
+    private boolean rightPressed = false;
 
     public GamePanel() {
         this.setBackground(Color.BLACK);
         player = new Player(100, 100, 50, 50);
+
+        this.setFocusable(true);
+        this.addKeyListener(this);
         //Hàm test di chuyển nva
         Timer timer = new Timer(16, e -> {
-            player.moveRight();
+            move();
             repaint();
         });
         timer.start();
+    }
+    private void move() {
+        if (leftPressed) {
+            player.moveLeft();
+        }
+
+        if (rightPressed) {
+            player.moveRight();
+        }
     }
     @Override
     protected void paintComponent(Graphics g) {
@@ -30,5 +46,30 @@ public class GamePanel extends JPanel {
                 player.getWidth(),
                 player.getHeight()
         );
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
+            leftPressed = true;
+        }
+        if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
+            rightPressed = true;
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT) {
+            leftPressed = false;
+        }
+        if (key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT) {
+            rightPressed = false;
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
     }
 }
