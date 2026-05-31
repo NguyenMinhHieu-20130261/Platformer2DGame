11baf4bb87062f372c35524b163d61d0d6c023d3
Nguyen Minh Hieu <20130261@st.hcmuaf.edu.vn>
2026-05-30 20:44:24 -1000

    tạo model player

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,16 +1,27 @@
 import javax.swing.JPanel;
+
+import model.Player;
+
 import java.awt.Color;
 import java.awt.Graphics;
 
 public class GamePanel extends JPanel {
 
+    private Player player;
+
     public GamePanel() {
         this.setBackground(Color.BLACK);
+        player = new Player(100, 100, 50, 50);
     }
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillRect(100, 100, 50, 50);
+        g.fillRect(
+                player.getX(),
+                player.getY(),
+                player.getWidth(),
+                player.getHeight()
+        );
     }
 }
